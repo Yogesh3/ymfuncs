a4f9edeb60d98778032584828905f56b5b414021
Yogesh3 <ymehta630@gmail.com>
2023-11-02 10:44:58 -0700

    added separate getClassyKappa function

--- myfuncs/util.py
+++ myfuncs/util.py
@@ -1,6 +1,6 @@
 import numpy as np
 from astropy import units as u
-from falafel.utils import config
+# from falafel.utils import config
 from orphics import cosmology, maps as omaps 
 from classy_sz import Class
 
@@ -184,7 +184,104 @@ def get_theory_dicts(nells=None,lmax=9000,grad=True):
 #         output = np.sum(mask1**n1 * mask2**n2 * pmap) /np.pi / 4.
 #     return output
 
-def getClassyCIB(spectra, nu_list, params={}, emulFlag=False, kappaFlag=False):
+def getClassyKappa(params={}):
+    """
+    Return kappa auto spectrum from classy_sz.
+
+    Parameters
+    ----------
+    params : dict, optional
+        Any parameters you want to pass to classy_sz, by default {}
+
+    Returns
+    -------
+    ells, dict
+        Ells and dictionary of kappa auto spectrum. Keys are 'hm' for halo model-based nonlinear power spectrum and 'hf' for HaloFit-based nonlinear power spectrum.
+    """
+    #Parameters for Cosmology Planck 14, https://arxiv.org/pdf/1303.5076.pdf, best-fit
+    p14_dict={}
+    p14_dict['h'] = 0.6711 
+    p14_dict['omega_b'] = 0.022068
+    p14_dict['Omega_cdm'] = 0.3175 - 0.022068/p14_dict['h']/p14_dict['h']
+    p14_dict['A_s'] = 2.2e-9
+    p14_dict['n_s'] = .9624
+    p14_dict['k_pivot'] = 0.05
+    p14_dict['tau_reio'] = 0.0925
+    p14_dict['N_ncdm'] = 1
+    p14_dict['N_ur'] = 0.00641
+    p14_dict['deg_ncdm'] = 3
+    p14_dict['m_ncdm'] = 0.02
+    p14_dict['T_ncdm'] = 0.71611
+
+    p_hm_dict = {}
+
+    #Grid Parameters
+    # Redshift bounds
+    p_hm_dict['z_min'] = 0.07
+    p_hm_dict['z_max'] = 6. # fiducial for MM20 : 6
+    p_hm_dict['freq_min'] = 10.
+    p_hm_dict['freq_max'] = 5e4 # fiducial for MM20 : 6
+    p_hm_dict['z_max_pk'] = p_hm_dict['z_max']
+
+    #Precision Parameters
+    # Precision for redshift integal
+    p_hm_dict['redshift_epsabs'] = 1e-40#1.e-40
+    p_hm_dict['redshift_epsrel'] = 1e-4#1.e-10 # fiducial value 1e-8
+    # Precision for mass integal
+    p_hm_dict['mass_epsabs'] = 1e-40 #1.e-40
+    p_hm_dict['mass_epsrel'] = 1e-4#1e-10
+    # Multipole array
+    p_hm_dict['dlogell'] = 1
+    p_hm_dict['ell_max'] = 3968.0
+    p_hm_dict['ell_min'] = 2.0
+
+    #More Params
+    p_hm_dict['ndim_masses'] = 150 # important 128 is default ccl value
+    p_hm_dict['ndim_redshifts'] = 150
+    p_hm_dict['non_linear'] = 'halofit'
+    p_hm_dict['perturb_sampling_stepsize']  = 0.005
+    p_hm_dict['k_max_tau0_over_l_max'] = 5.
+
+    #Spectra 
+    outspec = {}
+    outspec['output'] = 'lens_lens_1h,lens_lens_2h,len_lens_hf'
+    if 'output' in params.keys():
+        outspec['output'] = outspec['output'] + '_' + params.pop('output')
+
+    #Create Class Object
+    M = Class()
+
+    #Add Parameters
+    M.set(p14_dict)
+    M.set(p_hm_dict)
+    M.set(outspec)
+    if params:
+        M.set(params)
+        
+    #Compute Power Spectra
+    M.compute()
+    Dl_kappa_dict = M.cl_kk()
+    ells = np.array(Dl_kappa_dict['ell'])
+
+    Cls_kappa = {}
+
+    #Get Halo Model Kappa
+    Dl_kappa_hm = np.array(Dl_kappa_dict['1h']) + np.array(Dl_kappa_dict['2h'])
+    Cl_kappa_hm = dl2cl(Dl_kappa_hm, ells= ells)
+    Cls_kappa['hm'] = Cl_kappa_hm
+
+    #Get Halofit Kappa
+    Cl_hf = dl2cl(Dl_kappa_dict['hf'], ells= ells)
+    Cls_kappa['hf'] = Cl_hf
+
+    M.struct_cleanup()
+    M.empty()
+
+    return ells, Cls_kappa
+
+
+    
+def getClassyCIB(spectra, nu_list, params={}, emulFlag=False):
     """Wrapper for classy_sz calculations of CIB auto and CIB x lensing theory spectra.
 
     Parameters
@@ -197,13 +294,11 @@ def getClassyCIB(spectra, nu_list, params={}, emulFlag=False, kappaFlag=False):
         Dictionary of classy_sz parameters, by default empty
     emulFlag : bool, optional
         Use the cosmopower emulators?, by default False
-    kappaFlag : bool, optional
-        return kappa autospectrum?, by defualt False
 
     Returns
     -------
     ells, Cls_dict
-        Array of ells and a dictionary of Cls. The keys are 'auto' and 'cross', and each of those entries is itself a dictionary indexed by observing frequency as a string.
+        Array of ells and a dictionary of Cls. The keys are 'auto' and 'cross' (and optionally 'kappa'), and each of those entries is itself a dictionary indexed by observing frequency as a string (and optionally by 'hm' or 'hf' for kappa).
     """
 
     if spectra.lower() not in ['both', 'cross', 'auto']:
@@ -265,14 +360,14 @@ def getClassyCIB(spectra, nu_list, params={}, emulFlag=False, kappaFlag=False):
 
     #CIB Parameters
     p_CIB_dict = {}
-    p_CIB_dict['alpha'] =  0.36
-    p_CIB_dict['T_o'] = 24.4
-    p_CIB_dict['beta'] = 1.75
-    p_CIB_dict['gamma'] = 1.7
-    p_CIB_dict['delta'] = 3.6
-    p_CIB_dict['M_eff'] = 10**12.6
-    p_CIB_dict['L_o'] = 6.4e-8
-    p_CIB_dict['sigma_sq'] = 0.5
+    p_CIB_dict['Redshift evolution of dust temperature'] =  0.36
+    p_CIB_dict['Dust temperature today in Kelvins'] = 24.4
+    p_CIB_dict['Emissivity index of sed'] = 1.75
+    p_CIB_dict['Power law index of SED at high frequency'] = 1.7
+    p_CIB_dict['Redshift evolution of L - M normalisation'] = 3.6
+    p_CIB_dict['Most efficient halo mass in Msun'] = 10**12.6
+    p_CIB_dict['Normalisation of L - M relation in [Jy MPc2/Msun]'] = 6.4e-8
+    p_CIB_dict['Size of of halo masses sourcing CIB emission'] = 0.5
 
     # nu_list = [353,545,857]
     nu_list_str = str(nu_list)[1:-1]  # Note: this must be a single string, not a list of strings!
@@ -315,44 +410,6 @@ def getClassyCIB(spectra, nu_list, params={}, emulFlag=False, kappaFlag=False):
     p_freq_dict['cib_Snu_cutoff_list [mJy]'] = str(list(cib_flux_list))[1:-1]
     p_freq_dict['has_cib_flux_cut'] = 1
 
-    # M.set({# class_sz parameters:
-    #     'output':'lens_cib_1h,lens_cib_2h', 
-        
-    #     #CIB Parameters
-    #     'Redshift evolution of dust temperature' :  0.36,
-    #     'Dust temperature today in Kelvins' : 24.4,
-    #     'Emissivity index of sed' : 1.75,
-    #     'Power law index of SED at high frequency' : 1.7,
-    #     'Redshift evolution of L - M normalisation' : 3.6,
-    #     'Most efficient halo mass in Msun' : 10.**12.6,
-    #     'Normalisation of L - M relation in [Jy MPc2/Msun]' : 6.4e-8,
-    #     'Size of of halo masses sourcing CIB emission' : 0.5,
-
-    #     #M_min_HOD is the threshold above which nc = 1:
-    #     'M_min_HOD' : 10.**10,
-
-    #     'M_min' : 1e10*common_settings['h'],
-    #     'M_max' : 1e16*common_settings['h'],
-    #     'z_min' : 0.06,
-    #     'z_max' : 15,
-
-    #     ### Precision
-    #     #redshift_epsabs : 1.0e-40
-    #     #redshift_epsrel : 0.0005
-    #     #mass_epsabs : 1.0e-40
-    #     #mass_epsrel : 0.0005
-    #     'dell' : 64,
-    #     #multipoles_sz : 'ell_mock'
-    #     'ell_max' : 3968.0,
-    #     'ell_min' : 2.0,
-    #     'ndim_masses' : 100,
-    #     'ndim_redshifts' : 100,
-        
-    #     'cib_frequency_list_num' : Nfreq,
-    #     #'cib_frequency_list_in_GHz' : '217,353,545,857,3000',
-    #     'cib_frequency_list_in_GHz' : '353, 545, 857'
-    #     })
-
     #Create Class Object
     M = Class()
     
@@ -362,8 +419,6 @@ def getClassyCIB(spectra, nu_list, params={}, emulFlag=False, kappaFlag=False):
         outspec.append('cib_cib_1h,cib_cib_2h')
     if spectra.lower() == 'both' or spectra == 'cross':
         outspec.append('lens_cib_1h,lens_cib_2h')
-    if kappaFlag:
-        outspec.append('lens_lens_1h,lens_lens_2h')
     M.set({'output': ','.join(outspec)})
 
     #Add Parameters
@@ -374,6 +429,7 @@ def getClassyCIB(spectra, nu_list, params={}, emulFlag=False, kappaFlag=False):
     if params:
         M.set(params)
             
+    # import pdb;pdb.set_trace()
     #Compute Spectra
     if emulFlag:
         M.compute_class_szfast()
@@ -386,8 +442,6 @@ def getClassyCIB(spectra, nu_list, params={}, emulFlag=False, kappaFlag=False):
         Dl_spectra['auto'] = M.cl_cib_cib()
     if spectra.lower() == 'both' or spectra == 'cross':
         Dl_spectra['cross'] = M.cl_lens_cib()
-    M.struct_cleanup()
-    M.empty()
 
     ells = []
     Cls_dict = {}
@@ -412,11 +466,8 @@ def getClassyCIB(spectra, nu_list, params={}, emulFlag=False, kappaFlag=False):
             #Save Spectra
             Cls_dict[spec_key][nu] = Cl_total
         
-    #Get Kappa Autospectrum
-    if kappaFlag:
-        Dl_phi = M.cl_lens_lens()
-        Cl_phi = dl2cl(Dl_phi, ells= ells)
-        Cls_dict['lens'] = Cl_phi
+    M.struct_cleanup()
+    M.empty()
 
     return ells, Cls_dict
     
