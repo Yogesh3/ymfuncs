788c8967eb80bb5d7b84c9b255b999b480820109
Yogesh <ymehta3@niagara>
2025-10-08 23:10:48 -0400

    added trillium paths

--- myfuncs/util.py
+++ myfuncs/util.py
@@ -8,7 +8,7 @@ from orphics import cosmology, maps as omaps
 import healpy as hp
 import yaml
 import myfuncs as ym
-from classy_sz import Class
+# from classy_sz import Class
 
 def round_percent(number, nth_non_nine= 1):
     """
